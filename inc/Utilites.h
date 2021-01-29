5f6c23b327ee23bb0121beddb6171a5ca854be05
Alex Curtiss <alexander.curtiss@u.northwestern.edu>
2021-01-29 10:10:18 -0600

    improve barometer initialization rate. fix broadcast data ranges

--- inc/Utilites.h
+++ inc/Utilites.h
@@ -3,9 +3,20 @@
 
 #include <math.h>
 
-double round(double val)
+namespace Utilities
 {
-    return floor(d + 0.5)
+
+    double map_range(double val, double input_start, double input_end, double output_start, double output_end)
+    {
+        double slope = (output_end - output_start) / (input_end - input_start);
+        return output_start + slope * (val - input_start);
+    }
+
+    double round(double val)
+    {
+        return floor(d + 0.5)
+    }
+
 }
 
 #endif // UTILITIES_H_
