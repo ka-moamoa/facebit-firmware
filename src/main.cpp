d90c13cf4fdaa8b95cd8b1119452d9fa1967cfa5
Alex Curtiss <alexander.curtiss@u.northwestern.edu>
2021-02-12 14:37:19 -0600

    reduce memory usage

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,11 @@ DigitalOut led(LED1);
 BusControl *bus_control = BusControl::get_instance();
 
 SWO_Channel SWO; // for SWO logging
-I2C i2c(I2C_SDA0, I2C_SCL0);
+// I2C i2c(I2C_SDA0, I2C_SCL0);
 SPI spi(SPI_MOSI, SPI_MISO, SPI_SCK);
 
-Si7051 temp(&i2c);
-Barometer barometer(&spi, BAR_CS, BAR_DRDY);
+// Si7051 temp(&i2c);
+// Barometer barometer(&spi, BAR_CS, BAR_DRDY);
 BCG bcg(&spi, IMU_INT1, IMU_CS);
 
 SWO_Channel swo("channel");
@@ -53,6 +53,12 @@ Thread *thread1;
 // Thread *thread2;
 Thread *thread3;
 
+namespace smartppe
+{
+    BLE &ble = BLE::Instance();
+}
+
+GattServerProcess ble_process(event_queue, smartppe::ble);
 SmartPPEService smart_ppe_ble;
 
 Thread t1;
@@ -67,72 +73,72 @@ void led_thread()
     }
 }
 
-void sensor_thread()
-{   
-    bus_control->spi_power(true);
-    spi.frequency(5000000);
+// void sensor_thread()
+// {   
+//     bus_control->spi_power(true);
+//     spi.frequency(5000000);
 
-    bus_control->i2c_power(true);
+//     bus_control->i2c_power(true);
 
-    ThisThread::sleep_for(1000ms);
+//     ThisThread::sleep_for(1000ms);
 
-    temp.initialize();
+//     temp.initialize();
     
-    if (!barometer.initialize() || !barometer.set_fifo_full_interrupt(true))
-    {
-        LOG_WARNING("%s", "barometer failed to initialize");
-        while(1) {};
-    }
+//     if (!barometer.initialize() || !barometer.set_fifo_full_interrupt(true))
+//     {
+//         LOG_WARNING("%s", "barometer failed to initialize");
+//         while(1) {};
+//     }
 
-    if (!barometer.set_pressure_threshold(1100) || !barometer.enable_pressure_threshold(true, true, false))
-    {
-        LOG_WARNING("%s", "barometer setup failed");
-        while(1) {};
-    }
+//     if (!barometer.set_pressure_threshold(1100) || !barometer.enable_pressure_threshold(true, true, false))
+//     {
+//         LOG_WARNING("%s", "barometer setup failed");
+//         while(1) {};
+//     }
 
-    while(1)
-    {
-        barometer.update();
-        temp.update();
+//     while(1)
+//     {
+//         barometer.update();
+//         temp.update();
 
-        if (barometer.get_buffer_full())
-        {
-            LOG_DEBUG("barometer buffer full. %u elements. timestamp = %llu. measurement frequency x100 = %lu", barometer.get_pressure_buffer_size(), barometer.get_delta_timestamp(false), barometer.get_measurement_frequencyx100());
+//         if (barometer.get_buffer_full())
+//         {
+//             LOG_DEBUG("barometer buffer full. %u elements. timestamp = %llu. measurement frequency x100 = %lu", barometer.get_pressure_buffer_size(), barometer.get_delta_timestamp(false), barometer.get_measurement_frequencyx100());
             
-            smart_ppe_ble.updatePressure(
-                barometer.get_delta_timestamp(true), 
-                barometer.get_measurement_frequencyx100(), 
-                barometer.get_pressure_array(), 
-                barometer.get_pressure_buffer_size());
+//             smart_ppe_ble.updatePressure(
+//                 barometer.get_delta_timestamp(true), 
+//                 barometer.get_measurement_frequencyx100(), 
+//                 barometer.get_pressure_array(), 
+//                 barometer.get_pressure_buffer_size());
             
-            smart_ppe_ble.updateDataReady(smart_ppe_ble.PRESSURE);
+//             smart_ppe_ble.updateDataReady(smart_ppe_ble.PRESSURE);
 
 
-            barometer.clear_buffers();
-        }
+//             barometer.clear_buffers();
+//         }
 
-        if (temp.getBufferFull())
-        {
-            LOG_DEBUG("temperature buffer full. %u elements. timestamp = %llu. measurement frequency x100 = %lu", temp.getBufferSize(), temp.getDeltaTimestamp(false), temp.getFrequencyx100());
+//         if (temp.getBufferFull())
+//         {
+//             LOG_DEBUG("temperature buffer full. %u elements. timestamp = %llu. measurement frequency x100 = %lu", temp.getBufferSize(), temp.getDeltaTimestamp(false), temp.getFrequencyx100());
 
-            smart_ppe_ble.updateTemperature(
-                temp.getDeltaTimestamp(true), 
-                temp.getFrequencyx100(),
-                temp.getBuffer(),
-                temp.getBufferSize());
+//             smart_ppe_ble.updateTemperature(
+//                 temp.getDeltaTimestamp(true), 
+//                 temp.getFrequencyx100(),
+//                 temp.getBuffer(),
+//                 temp.getBufferSize());
 
-            smart_ppe_ble.updateDataReady(smart_ppe_ble.TEMPERATURE);
+//             smart_ppe_ble.updateDataReady(smart_ppe_ble.TEMPERATURE);
 
-            temp.clearBuffer();
-        }
+//             temp.clearBuffer();
+//         }
 
-        ThisThread::sleep_for(1ms);
-    }
-}
+//         ThisThread::sleep_for(1ms);
+//     }
+// }
 
-void bcg_thread(GattServerProcess *ble_process)
+void bcg_thread()
 {   
-    while(!ble_process->is_connected())
+    while(!ble_process.is_connected())
     {
         ThisThread::sleep_for(1s);
     }
@@ -170,9 +176,6 @@ void bcg_thread(GattServerProcess *ble_process)
 int main()
 {
     swo.claim();
-
-    BLE &ble = BLE::Instance();
-    GattServerProcess ble_process(event_queue, ble);
     
     bus_control->init();
     ThisThread::sleep_for(10ms);
@@ -183,7 +186,7 @@ int main()
 
     thread1->start(led_thread);
     // thread2->start(sensor_thread);
-    thread3->start(callback(bcg_thread, &ble_process));
+    thread3->start(bcg_thread);
 
     ble_process.on_init(callback(&smart_ppe_ble, &SmartPPEService::start));
 
