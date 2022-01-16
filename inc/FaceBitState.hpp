229375368b082846bd5eb2126f7bb0dc6445b0b0
Alex Curtiss <alexander.curtiss@u.northwestern.edu>
2022-01-16 11:51:55 -0600

    add license notices

--- inc/FaceBitState.hpp
+++ inc/FaceBitState.hpp
@@ -1,3 +1,25 @@
+/**
+ * @file FaceBitState.hpp
+ * @author Alexander Curtiss apcurtiss@gmail.com
+ * @brief 
+ * @version 0.1
+ * @date 2022-01-16
+ * 
+ * @copyright Copyright (c) 2022 Ka Moamoa
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3 of the license.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef FACEBITSTATE_H_
 #define FACEBITSTATE_H_
 
